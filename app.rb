db6fe80efe9fc132ea6ee2dc57b1b800f8805d83
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-05-19 19:05:44 +0000

    Another correction attempt

--- app.rb
+++ app.rb
@@ -1,14 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/pmt/with/form")
+get("/monthly/payment") do
+ erb(:monthly_payment)
+end
+
+get("/payment/results")
   @apr_input = (params.fetch("apr_input").to_f / 100) / 12
   @years_input = params.fetch("years_input").to_i * 12
   @principal_input = params.fetch("principal_input").to_f
 
   @numerator = @apr_input * @principal_input
   @denominator = 1 - (1 + @apr_input) ** -@years_input
-  @monthly_payment = .to_fs(:percentage, { :precision => 4 } )
+  payment = @numerator / @denominator
+  @monthly_payment = payment.to_fs(:percentage, { :precision => 4 } )
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
 end
 
 get("/random/results") do
@@ -20,11 +30,19 @@ get("/random/results") do
   erb(:random_results)
 end
 
+get("square/root") do
+  erb(:square_root)
+end
+
 get("/square/root/results") do
   @the_num = params.fetch("num_to_square").to_f
 
   @square_root = @the_num / @the_num
   erb(:square_root_results)
+end
+
+get("/square/new") do
+  erb(:square_new)
 end
 
 get ("/square/results") do
