e0aec691fc3dcde359bb8462a7e2d6931a98fc01
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-05-19 17:43:25 +0000

    Added elements for each view

--- app.rb
+++ app.rb
@@ -1,12 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/howdy") do
-erb(:hello)
+get("/random/new") do
+
+  erb(:random_new)
 end
 
-get("/goodbye") do
-  erb(:bye)
+get("/square/root") do
+  @the_num = params.fetch("num_to_square").to_f
+
+  @the_result = @the_num / @the_num
+  erb(:square_root)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get '/square/results' do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+  erb(:square_results)
 end
 
 get("/") do
